6e98ae2bbbd0f76545c117fe4226ed91597e7369
aidanw <aidanweatherill@hotmail.com>
2018-09-09 20:19:57 +0100

    ran 'react-native link' then reverted the ios changes.
    Also manual steps from https://stackoverflow.com/questions/41716231/android-warning-native-component-for-airmap-does-not-exist/50777873#50777873

--- App.js
+++ App.js
@@ -5,27 +5,44 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View, Dimensions } from "react-native";
+import MapView, { Polygon } from "react-native-maps";
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android: "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+const { width, height } = Dimensions.get("window");
+const halfHeight = height / 2;
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <MapView
+          style={styles.map}
+          provider={"google"}
+          rotateEnabled={false}
+          showsUserLocation={true}
+          showsMyLocationButton={true}
+          showsCompass={true}
+          toolbarEnabled={true}
+          mapType={"satellite"}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 45,
+            longitudeDelta: 45
+          }}
+        >
+          <Polygon
+            key={1}
+            coordinates={[
+              { latitude: 40, longitude: 2 },
+              { latitude: 45, longitude: -5 },
+              { latitude: 50, longitude: 4 }
+            ]}
+            strokeColor="#F00"
+            fillColor="rgba(255,0,0,0.5)"
+            strokeWidth={1}
+          />
+        </MapView>
       </View>
     );
   }
@@ -38,14 +55,14 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#F5FCFF"
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
+  map: {
+    width,
+    height: halfHeight
   }
 });
