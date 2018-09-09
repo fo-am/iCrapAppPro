495f5c38864940c653f800c17b99464ea9697855
aidanw <aidanweatherill@hotmail.com>
2018-09-09 23:39:05 +0100

    can add polygons!

--- App.js
+++ App.js
@@ -5,16 +5,80 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View, Dimensions } from "react-native";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  StatusBar,
+  TouchableOpacity
+} from "react-native";
 import MapView, { Polygon } from "react-native-maps";
 
 const { width, height } = Dimensions.get("window");
 const halfHeight = height / 2;
 
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = 0.0;
+const LATITUDE_DELTA = 50;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+let id = 0;
+
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      },
+      polygons: [],
+      editing: null
+    };
+  }
+  finish() {
+    const { polygons, editing } = this.state;
+    this.setState({
+      polygons: [...polygons, editing],
+      editing: null
+    });
+  }
+  onPress(e) {
+    const { editing } = this.state;
+    if (!editing) {
+      this.setState({
+        editing: {
+          id: id++,
+          coordinates: [e.nativeEvent.coordinate]
+        }
+      });
+    } else {
+      this.setState({
+        editing: {
+          ...editing,
+          coordinates: [...editing.coordinates, e.nativeEvent.coordinate]
+        }
+      });
+    }
+  }
+
   render() {
+    const mapOptions = {
+      scrollEnabled: true
+    };
+
+    if (this.state.editing) {
+      mapOptions.scrollEnabled = false;
+      mapOptions.onPanDrag = e => this.onPress(e);
+    }
     return (
       <View style={styles.container}>
+        <StatusBar />
+        <Text>Top</Text>
         <MapView
           style={styles.map}
           provider={"google"}
@@ -24,25 +88,34 @@ export default class App extends Component {
           showsCompass={true}
           toolbarEnabled={true}
           mapType={"satellite"}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 45,
-            longitudeDelta: 45
-          }}
+          initialRegion={this.state.region}
+          onPress={e => this.onPress(e)}
         >
-          <Polygon
-            key={1}
-            coordinates={[
-              { latitude: 40, longitude: 2 },
-              { latitude: 45, longitude: -5 },
-              { latitude: 50, longitude: 4 }
-            ]}
-            strokeColor="#F00"
-            fillColor="rgba(255,0,0,0.5)"
-            strokeWidth={1}
-          />
+          {this.state.polygons.map(polygon => (
+            <Polygon
+              key={polygon.id}
+              coordinates={polygon.coordinates}
+              strokeColor="#F00"
+              fillColor="rgba(255,0,0,0.5)"
+              strokeWidth={1}
+            />
+          ))}
+          {this.state.editing && (
+            <Polygon
+              key={this.state.editing.id}
+              coordinates={this.state.editing.coordinates}
+              strokeColor="#000"
+              fillColor="rgba(255,0,0,0.5)"
+              strokeWidth={1}
+            />
+          )}
         </MapView>
+        <TouchableOpacity
+          onPress={() => this.finish()}
+          style={[styles.bubble, styles.button]}
+        >
+          <Text>Finish + {id}</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -51,18 +124,25 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "#333"
   },
   map: {
     width,
     height: halfHeight
+  },
+  topBar: {
+    height: Platform.OS === "ios" ? 20 : StatusBar.currentHeight
+  },
+  bubble: {
+    backgroundColor: "rgba(255,255,255,0.7)",
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20
+  },
+  button: {
+    width: 80,
+    paddingHorizontal: 12,
+    alignItems: "center",
+    marginHorizontal: 10
   }
 });
