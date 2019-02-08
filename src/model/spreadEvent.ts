e0bd4f62b067e4f8412191ae8ea1c060b6c1f32e
aidanw <aidanweatherill@hotmail.com>
2019-02-08 00:15:25 +0000

    update model to match database

--- src/model/spreadEvent.ts
+++ src/model/spreadEvent.ts
@@ -10,6 +10,18 @@ export default class SpreadEvent {
     @observable public quality: string = "";
     @observable public applicationType: string = "";
     @observable public amount: number = 0;
+    @observable public nutrientsN: number = 0;
+    @observable public nutrientsP: number = 0;
+    @observable public nutrientsK: number = 0;
+    @observable public requireN: number = 0;
+    @observable public requireP: number = 0;
+    @observable public requireK: number = 0;
+    @observable public sns: number = 0;
+    @observable public soil: string = "";
+    @observable public size: number = 0;
+    @observable public season: string = "";
+    @observable public crop: string = "";
+
     // copy numbers from field and fix calculated values.
     constructor() {
         this.date = moment(Date.now());
