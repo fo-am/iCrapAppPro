31c1852c1bf011240eb0bfd2ab831e6a5916bfc6
aidanw <aidanweatherill@hotmail.com>
2019-03-01 17:59:02 +0000

    add email sending ability

--- android/app/src/main/java/com/farm_crap_app/MainApplication.java
+++ android/app/src/main/java/com/farm_crap_app/MainApplication.java
@@ -3,6 +3,7 @@ package com.farm_crap_app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.chirag.RNMail.RNMail;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNMail(),
             new RNGestureHandlerPackage(),
             new VectorIconsPackage(),
             new MapsPackage(),
