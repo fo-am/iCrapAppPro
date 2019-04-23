f95f776f06dea98a75b0706d86e10f629bbba30b
aidanw <aidanweatherill@hotmail.com>
2019-04-23 15:45:10 +0100

    force box for farms list

--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -1,6 +1,6 @@
 import { inject, observer } from "mobx-react/native";
 import React, { Component } from "react";
-import { FlatList, ScrollView, Text, View } from "react-native";
+import { Dimensions, FlatList, ScrollView, Text, View } from "react-native";
 import { Button } from "react-native-elements";
 import { NavigationScreenProp, SafeAreaView } from "react-navigation";
 import { database } from "./database/Database";
@@ -32,91 +32,95 @@ export default class HomeScreen extends Component<
     return (
       <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
         <View style={styles.container}>
-          <Text style={styles.text}>Add a farm</Text>
-          <Button
-            buttonStyle={styles.roundButton}
-            titleStyle={styles.buttonText}
-            onPress={() =>
-              this.props.navigation.navigate("Farm", {
-                farmKey: undefined
-              })
-            }
-            title="Add a Farm"
-          />
           <ScrollView>
-            <FlatList<Farm>
-              data={this.props.FarmStore.farms.slice()}
-              keyExtractor={item => item.key}
-              renderItem={({ item }) => (
-                <Button
-                  titleStyle={styles.buttonText}
-                  buttonStyle={[
-                    styles.roundButton,
-                    styles.bgColourBlue,
-                    { marginLeft: "5%", marginRight: "5%" }
-                  ]}
-                  onPress={() => {
-                    this.props.navigation.navigate("Farm", {
-                      farmKey: item.key
-                    });
-                  }}
-                  title={item.name}
-                />
-              )}
+            <Text style={styles.text}>Add a farm</Text>
+            <Button
+              buttonStyle={styles.roundButton}
+              titleStyle={styles.buttonText}
+              onPress={() =>
+                this.props.navigation.navigate("Farm", {
+                  farmKey: undefined
+                })
+              }
+              title="Add a Farm"
+            />
+            <View
+              style={{ flex: 1, height: Dimensions.get("window").height * 0.5 }}
+            >
+              <FlatList<Farm>
+                style={{ flex: 1 }}
+                data={this.props.FarmStore.farms.slice()}
+                keyExtractor={item => item.key}
+                renderItem={({ item }) => (
+                  <Button
+                    titleStyle={styles.buttonText}
+                    buttonStyle={[
+                      styles.roundButton,
+                      styles.bgColourBlue,
+                      { marginLeft: "5%", marginRight: "5%" }
+                    ]}
+                    onPress={() => {
+                      this.props.navigation.navigate("Farm", {
+                        farmKey: item.key
+                      });
+                    }}
+                    title={item.name}
+                  />
+                )}
+              />
+            </View>
+            <Text style={styles.text}>Settings</Text>
+            <Button
+              buttonStyle={styles.roundButton}
+              titleStyle={styles.buttonText}
+              onPress={() => this.props.navigation.navigate("Settings")}
+              title="Settings"
+            />
+
+            <Button
+              buttonStyle={styles.roundButton}
+              titleStyle={styles.buttonText}
+              onPress={() => this.props.navigation.navigate("Calculator")}
+              title="Calculator"
+            />
+
+            <Text style={styles.text}>Manure</Text>
+            <Button
+              buttonStyle={styles.roundButton}
+              titleStyle={styles.buttonText}
+              onPress={() => this.props.navigation.navigate("CustomManure")}
+              title="Add a new manure"
+            />
+            <View>
+              <FlatList<Manure>
+                data={this.props.ManureStore.manures.slice()}
+                keyExtractor={item => item.key}
+                renderItem={({ item }) => (
+                  <Button
+                    style={styles.roundButton}
+                    onPress={() => {
+                      this.props.navigation.navigate("CustomManure", {
+                        manure: item
+                      });
+                    }}
+                    title={item.name}
+                  />
+                )}
+              />
+            </View>
+            <Button
+              buttonStyle={styles.roundButton}
+              titleStyle={styles.buttonText}
+              onPress={() => this.props.navigation.navigate("Export")}
+              title="Export Data"
+            />
+            <Button
+              buttonStyle={styles.roundButton}
+              titleStyle={styles.buttonText}
+              onPress={() => this.clearStore()}
+              title="Clear Store"
             />
           </ScrollView>
-
-          <Text style={styles.text}>Settings</Text>
-          <Button
-            buttonStyle={styles.roundButton}
-            titleStyle={styles.buttonText}
-            onPress={() => this.props.navigation.navigate("Settings")}
-            title="Settings"
-          />
-
-          <Button
-            buttonStyle={styles.roundButton}
-            titleStyle={styles.buttonText}
-            onPress={() => this.props.navigation.navigate("Calculator")}
-            title="Calculator"
-          />
-
-          <Text style={styles.text}>Manure</Text>
-          <Button
-            buttonStyle={styles.roundButton}
-            titleStyle={styles.buttonText}
-            onPress={() => this.props.navigation.navigate("CustomManure")}
-            title="Add a new manure"
-          />
-          <ScrollView>
-            <FlatList<Manure>
-              data={this.props.ManureStore.manures.slice()}
-              keyExtractor={item => item.key}
-              renderItem={({ item }) => (
-                <Button
-                  style={styles.roundButton}
-                  onPress={() => {
-                    this.props.navigation.navigate("CustomManure", {
-                      manure: item
-                    });
-                  }}
-                  title={item.name}
-                />
-              )}
-            />
-          </ScrollView>
-          <Button
-            buttonStyle={styles.roundButton}
-            titleStyle={styles.buttonText}
-            onPress={() => this.props.navigation.navigate("Export")}
-            title="Export Data"
-          />
-          <Button
-            buttonStyle={styles.roundButton}
-            titleStyle={styles.buttonText}
-            onPress={() => this.clearStore()}
-            title="Clear Store"
-          />
         </View>
       </SafeAreaView>
     );
