c8de35419adbd06797b2ed1c326a57c9a888852f
Aidan weatherill <aidan@fo.am>
2019-10-03 15:48:17 +0100

    release 71: Import working

--- src/splashScreen/splashScreen.tsx
+++ src/splashScreen/splashScreen.tsx
@@ -127,7 +127,7 @@ export default class SplashScreen extends Component<
                   fontFamily: "ArimaMadurai-Regular"
                 }}
               >
-                Build 69
+                Build 71
               </Text>
             </View>
           </Content>
