1f9f42bdc713da4d63b6d47be76622f29b6c33f0
Aidan weatherill <aidan@fo.am>
2019-10-22 10:53:22 +0100

    Build 76

--- src/splashScreen/splashScreen.tsx
+++ src/splashScreen/splashScreen.tsx
@@ -118,7 +118,7 @@ export default class SplashScreen extends Component<
                   fontFamily: "ArimaMadurai-Regular"
                 }}
               >
-                Version 1.1
+                Version 1.2
               </Text>
               <Text
                 style={{
@@ -127,7 +127,7 @@ export default class SplashScreen extends Component<
                   fontFamily: "ArimaMadurai-Regular"
                 }}
               >
-                Build 72
+                Build 76
               </Text>
             </View>
           </Content>
